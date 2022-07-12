e02346179c6c049309ef6895618379878285438c
jon-tow <guac@ip-172-31-36-230.ec2.internal>
2022-07-12 02:34:14 +0000

    Update scripts for the pile

--- example_scripts/contriever.sh
+++ example_scripts/contriever.sh
@@ -13,8 +13,16 @@
 
 
 port=$(shuf -i 15000-16000 -n 1)
-TDIR="/private/home/gizacard/contriever/encoded-data"
-TRAINDATASETS="${TDIR}/wikisub/ ${TDIR}/cc-netsub/"
+# NOTE: TDIR must point to the directory specified in `tokenization_pile_script.sh`
+TDIR="/fsx/carper/contriever/encoded-data/bert-base-uncased"
+# If you want to run it on one shard of the training set, specify one dir:
+# `TRAINDATASETS="${TDIR}/NN"`
+# where "NN" can be anything from `00-29`.
+TRAINDATASETS=""
+for i in 0{0..9} {10..29} ; do
+    TRAINDATASETS+="${TDIR}/pile/{i}"
+done
+
 
 rmin=0.05
 rmax=0.5
@@ -30,6 +38,7 @@ mp=none
 
 name=$SLURM_JOB_ID-$POOL-rmin$rmin-rmax$rmax-T$T-$QSIZE-$MOM-$mo-$AUG-$PAUG
 
+# TODO: Rename environment path to your `contriever` python env.
 srun ~gizacard/anaconda3/envs/contriever/bin/python3 train.py \
         --model_path $mp \
         --sampling_coefficient $LC \
